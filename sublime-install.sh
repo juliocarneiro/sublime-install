9b19c09c8fe6706ff4a542d9beea547ebbd8620b
Julio Carneiro <juliocarneiro@gmail.com>
2016-05-09 13:15:51 +1200

    1.0

--- sublime-install.sh
+++ sublime-install.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 
 # By: Júlio Carneiro
 # http://github.com/juliocarneiro
@@ -27,39 +27,38 @@ echo -e " @@@@@@   @@@  @@@  @@@@@@@   @@@       @@@  @@@@@@@@@@   @@@@@@@@
 :    ::    :   :: : :       :      :   : :  : :: : :  : :: : :  
                                                                 "
 
-echo -e "Para instalar o Sublime escolha a opção de versão quando executar.\n\n./sublime-install.sh -3 = Para instalar o Sublime 3\n./sublime-install.sh -2 = para instalar o Sublime 2\n\n By tr3c3"
+echo -e "Para instalar o Sublime escolha a opção de versão quando executar.\n\n./sublime-install.sh -3 = Para instalar o Sublime 3\n./sublime-install.sh -2 = para instalar o Sublime 2\n\n1.0 by tr3c3\n"
 
 if [ "$1" = "-3" ]; then
-echo "Instalacao do Sublime Text 3"
-echo "Baixando Repositorio..."
+echo -e "\n============= Instalação do Sublime Text 3 ============="
+echo -e "\n============= Baixando Repositório... =============\n"
 
 sudo add-apt-repository ppa:webupd8team/sublime-text-3
 
-echo "Atualizando Repositorio..."
+echo -e "\n============= Atualizando Repositorio... =============\n"
 
 sudo apt-get update
 
-echo "Baixando e instalando o programa..."
+echo -e "\n============= Baixando e instalando o programa... =============\n"
 
 sudo apt-get install sublime-text
 
-echo "By tr3c3"
+echo "1.0 by Julio Carneiro"
 fi
 
 if [ "$1" = "-2" ]; then
-echo "Instalacao do Sublime Text 2"
-echo "Baixando Repositorio..."
+echo -e "\n============= Instalação do Sublime Text 2 =============\n"
+echo -e "\n============= Baixando Repositório... =============\n"
 
 sudo add-apt-repository ppa:webupd8team/sublime-text-2
 
-echo "Atualizando Repositorio..."
+echo -e "\n============= Atualizando Repositório... =============\n"
 
 sudo apt-get update
 
-echo "Baixando e instalando o programa..."
+echo -e "\n============= Baixando e instalando o programa... =============\n"
 
 sudo apt-get install sublime-text
 
-echo "By tr3c3"
 fi
 exit
